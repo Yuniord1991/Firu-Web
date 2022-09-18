8d515bf924a5a2d94cc4549c24a30d7860dbb1a6
Yuniord1991 <Yuniord1991@gmail.com>
2022-09-17 23:28:20 -0300

    - Añadido register - routing aprendido - limpieza del app.component.html - añadido archivos de fer para referencia - BORRAR LUEGO

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    data: { title: 'Inicio Sesion' }
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    data: { title: 'Registro' }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
